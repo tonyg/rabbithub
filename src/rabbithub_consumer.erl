4d38d5b843ce9df9599d374c72e83ab1372b4f02
brc859844 <brett.r.cameron@gmail.com>
2014-12-05 09:03:57 +1300

    Changes for NA

--- src/rabbithub_consumer.erl
+++ src/rabbithub_consumer.erl
@@ -26,7 +26,11 @@ really_init(Subscription = #rabbithub_subscription{resource = Resource}) ->
         {ok, Q = #amqqueue{pid = QPid}} ->
             ConsumerTag = rabbit_guid:binary(rabbit_guid:gen(), "amq.http.consumer"),
             MonRef = erlang:monitor(process, QPid),
-            rabbit_amqqueue:basic_consume(Q, false, self(), undefined, false, 0,
+            %% Note that prefetch count is set to 1. This will likely have some impact
+            %% on performance; however this is an internal consumer and HTTP POST
+            %% operations will invariably be the rate-limiting step. Setting prefetch
+            %% count to 1 allows better control over HTTP error handling.
+            rabbit_amqqueue:basic_consume(Q, false, self(), undefined, false, 1,
                                           ConsumerTag, false, [], undefined),
             {ok, #state{subscription = Subscription,
                         q_monitor_ref = MonRef,
@@ -55,6 +59,34 @@ handle_cast({deliver, _ConsumerTag, AckRequired,
                     ok
             end;
         {error, Reason} ->
+            case is_integer(Reason) of 
+                true ->
+                    %% If requeue_on_http_post_error is set to false then messages associated with
+                    %% failed HTTP POSTs will be dropped or published to a dead letter exchange (if
+                    %% one is associated with the queue in subscription queue in question). Note 
+                    %% that this ties in with setting the prefetch count to 1 (see above), which
+                    %% ensures that at most 2 messages will be rejected per error before the
+                    %% subscription gets deleted and the consumer processes is terminated. This
+                    %% setting is primarily intended for debugging purposes. For example, bad data
+                    %% might cause the receiving web application to break. For using this setting
+                    %% in conjunction with a dead letter exchange (and queue) it is possible to 
+                    %% capture the offending messages, rather than have them end up back on the 
+                    %% subscription queue and getting stuck in some sort of error loop.
+                    case application:get_env(rabbithub, requeue_on_http_post_error) of
+                       {ok, false} ->
+                           ok = rabbit_amqqueue:notify_sent(QPid, self()),
+                           case AckRequired of
+                               true ->
+                                   ok = rabbit_amqqueue:reject(QPid, false, [MsgId], self());
+                               false ->
+                                   ok
+                           end;
+                       _ ->
+                           ok
+                       end;
+                false ->
+                    ok
+            end,
             ok = rabbithub:error_and_unsub(Subscription,
                                            {rabbithub_consumer, http_post_failure, Reason})
     end,
